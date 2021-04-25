7d8c98536a579f4fcab32e98e666ef00aca2e790
William Pickering <wzpickering@gmail.com>
2021-04-24 17:42:46 -0700

    add modal

--- Frontend/src/components/App.jsx
+++ Frontend/src/components/App.jsx
@@ -4,49 +4,47 @@ import Footer from "./Footer";
 import InputTask from "./InputTask";
 import Task from "./Task";
 import ContextProvider from "./Context";
+import Modal from "./Modal";
 
 function App() {
-  // const [tasks, setTasks] = useState([]);
+  const [isOpen, toggleOpen] = useState(false);
 
-  
- 
-  // function addTask(inputTask) {
-  //   setTasks((prevTasks) => [...prevTasks, inputTask]);
-  // }
-
-  // function deleteTask(id){
-  //     setTasks((prevTasks)=>{
-  //         return prevTasks.filter((task, index) => {
-  //             return id !== index;
-  //         })
-  //     })
-  // }
+  const BUTTON_WRAPPER_STYLES = {
+    position: "relative",
+    zIndex: 1,
+  };
 
   return (
     <ContextProvider>
-    <div>
-      <Header />
-      <div className="task-form">
-        <div>
-          <h1>Daily Tasks</h1>
+      <div>
+        <Header />
+        <div style={BUTTON_WRAPPER_STYLES} onClick={()=>console.log("clicked")}> 
+        {/* dispite Modal being rendered seperately, it can pass function up to parent */}
+          <button onClick={() => toggleOpen(true)}>Add Project</button>
+          <Modal open={isOpen} onClose={() => toggleOpen(false)}></Modal>
         </div>
-        <InputTask /> 
-        <div>
-          <Task />
+
+        <div className="task-form">
+          <div>
+            <h1>Daily Tasks</h1>
+          </div>
+          <InputTask />
+          <div>
+            <Task />
+          </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
     </ContextProvider>
   );
 }
 
 export default App;
 
-        // <ul>
-        //     {tasks.map((task, index) => {
-        //       return <Task key={index} id={index} text={task} onDelete={deleteTask}/>; //
-        //     })}
-        //   </ul>
+// <ul>
+//     {tasks.map((task, index) => {
+//       return <Task key={index} id={index} text={task} onDelete={deleteTask}/>; //
+//     })}
+//   </ul>
 
-        // onAdd={addTask}
+// onAdd={addTask}
